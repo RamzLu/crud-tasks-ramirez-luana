086abfba4f9b0bd60e08dab2187f65c34fdd2594
Ramirez Luana <luanaabigail168@gmail.com>
2025-08-07 10:09:05 -0300

    hice la base de datos

--- main.js
+++ main.js
@@ -2,11 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import { routerUser } from "./src/routers/user.router.js";
+import { startDB } from "./src/config/database.js";
 const PORT = process.env.PORT;
 const app = express();
 
 app.use("/api", routerUser);
 
 app.listen(PORT, async () => {
+  await startDB();
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
