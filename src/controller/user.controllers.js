8f06a37033340df8db6c07bc4f992c2728a18c84
Ramirez Luana <luanaabigail168@gmail.com>
2025-08-07 11:20:56 -0300

    cree los controladores de user

--- src/controller/user.controllers.js
+++ src/controller/user.controllers.js
@@ -1,5 +1,75 @@
+import { User } from "../models/user.model.js";
+
 export const getAllUsers = async (req, res) => {
   try {
+    const user = await User.findAll(req.body);
+    return res.json(user);
+  } catch (error) {
+    res.status(501).json({
+      error: error.mesagge,
+    });
+  }
+};
+
+export const getUserById = async (req, res) => {
+  try {
+    const user = User.findByPk(req.body.id);
+    if (movie) {
+      res.json(user);
+    } else {
+      return res.status(404).json({
+        mesagge: "User not found.",
+      });
+    }
+  } catch (error) {
+    return res.status(501).json({
+      error: error.mesagge,
+    });
+  }
+};
+
+export const createUser = async (req, res) => {
+  console.log(req.body);
+  try {
+    const { name, email, password } = req.body;
+
+    console.log(name, email, password);
+    const user = await User.create(req.body);
+    console.log(user);
+    return res.json(user);
+  } catch (error) {
+    res.status(501).json({
+      error: error.mesagge,
+    });
+    console.log(error);
+  }
+};
+
+export const upDateUser = async (req, res) => {
+  try {
+    const [update] = User.update(req.body, { where: { id: req.params.id } });
+    if (update) {
+      const user = User.findByPk(req.params.id);
+      return res.status(201).json(user);
+    } else {
+      return res.status(404).json({
+        mesagge: "User not found.",
+      });
+    }
+  } catch (error) {
+    return res.status(501).json({
+      error: error.mesagge,
+    });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  try {
+    const deleted = User.destroy({ where: { id: req.params.id } });
+    if (deleted) {
+      return res.status(201).json({
+        mesagge: "User deleted successfully.",
+      });
+    }
   } catch (error) {}
-  //   res.send("Hello World");
 };
--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -1,5 +1,19 @@
 import { Router } from "express";
-import { getAllUsers } from "../controller/user.controllers.js";
+import {
+  getAllUsers,
+  createUser,
+  deleteUser,
+  getUserById,
+  upDateUser,
+} from "../controller/user.controllers.js";
 export const routerUser = Router();
 
-routerUser.get("/users", getAllUsers);
+routerUser.get("/user", getAllUsers);
+
+routerUser.post("/user", createUser);
+
+routerUser.get("/user/:id", getUserById);
+
+routerUser.put("/user/:id", upDateUser);
+
+routerUser.delete("/user/:id", deleteUser);
