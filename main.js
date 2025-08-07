fee7f70d114113cf2726269629547e2296049c4a
Ramirez Luana <luanaabigail168@gmail.com>
2025-08-07 11:20:32 -0300

    agregue el express.json

--- main.js
+++ main.js
@@ -3,9 +3,11 @@ import dotenv from "dotenv";
 dotenv.config();
 import { routerUser } from "./src/routers/user.router.js";
 import { startDB } from "./src/config/database.js";
+
 const PORT = process.env.PORT;
 const app = express();
 
+app.use(express.json());
 app.use("/api", routerUser);
 
 app.listen(PORT, async () => {
