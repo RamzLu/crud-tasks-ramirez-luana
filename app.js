4d810ebd540f2797c89ac9495f36f845af9ee290
Ramirez Luana <luanaabigail168@gmail.com>
2025-08-08 11:28:06 -0300

    cambie el nombre de main a app

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ import dotenv from "dotenv";
 dotenv.config();
 import { routerUser } from "./src/routers/user.router.js";
 import { startDB } from "./src/config/database.js";
+import { routerTask } from "./src/routers/task.router.js";
 
 const PORT = process.env.PORT;
 const app = express();
 
 app.use(express.json());
 app.use("/api", routerUser);
+app.use("/api", routerTask);
 
 app.listen(PORT, async () => {
   await startDB();
