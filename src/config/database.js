612355a921e4b295c037a282353afdf27faac432
Ramirez Luana <luanaabigail168@gmail.com>
2025-08-08 10:22:58 -0300

    borré el force

--- src/config/database.js
+++ src/config/database.js
@@ -15,7 +15,6 @@ export const startDB = async () => {
   try {
     await sequelize.authenticate();
     console.log("Connection has been established successfully.");
-    await sequelize.sync({ force: true });
   } catch (error) {
     console.error("Unable to connect to the database:", error);
   }
