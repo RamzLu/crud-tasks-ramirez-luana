fae24eb64f5ad96bb1461352c3b7dd7cc19015c9
Ramirez Luana <luanaabigail168@gmail.com>
2025-08-08 15:18:48 -0300

    hice las rutas task

--- src/routers/task.router.js
+++ src/routers/task.router.js
@@ -1,8 +1,15 @@
 import { Router } from "express";
+import {
+  createTask,
+  deleteTask,
+  getAllTask,
+  getTaskById,
+  upDatetask,
+} from "../controller/task.controller.js";
 export const routerTask = Router();
 
-routerTask.get("/task");
-routerTask.get("/task");
-routerTask.post("/task");
-routerTask.put("/task");
-routerTask.delete("/task");
+routerTask.get("/task", getAllTask);
+routerTask.post("/task", createTask);
+routerTask.get("/task/:id", getTaskById);
+routerTask.put("/task/:id", upDatetask);
+routerTask.delete("/task/:id", deleteTask);
