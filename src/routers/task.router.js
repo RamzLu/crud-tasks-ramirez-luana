acc4a7af9cb0ca6a1f0a34f23927b1e43d9f6fc8
Ramirez Luana <luanaabigail168@gmail.com>
2025-08-08 11:30:30 -0300

    creé el modelo y las rutas de task

---
+++ src/routers/task.router.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+export const routerTask = Router();
+
+routerTask.get("/task");
+routerTask.get("/task");
+routerTask.post("/task");
+routerTask.put("/task");
+routerTask.delete("/task");
