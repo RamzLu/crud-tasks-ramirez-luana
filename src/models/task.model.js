28880e25f1bc092d18c19dba1caf232c864a6ff6
Luana Ramirez <luanaabigail168@gmail.com>
2025-08-17 19:29:09 -0300

    hice las realciones de las task

--- src/models/task.model.js
+++ src/models/task.model.js
@@ -1,18 +1,30 @@
 import { DataTypes } from "sequelize";
 import { sequelize } from "../config/database.js";
+import { User } from "./user.model.js";
 
-export const Task = sequelize.define("Task", {
-  title: {
-    type: DataTypes.STRING(100),
-    unique: true,
-    allowNull: false,
+export const Task = sequelize.define(
+  "Task",
+  {
+    title: {
+      type: DataTypes.STRING(100),
+      unique: true,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    isComplete: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
   },
-  description: {
-    type: DataTypes.STRING(100),
-    allowNull: false,
-  },
-  isComplete: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: false,
-  },
-});
+  {
+    timestamps: false,
+  }
+);
+
+// un usuario puede tener muchas tareas
+User.hasMany(Task, { foreignKey: "user_id" });
+// las tareas le pueden pertenecer a un único usuario
+Task.belongsTo(User, { foreignKey: "user_id" });
