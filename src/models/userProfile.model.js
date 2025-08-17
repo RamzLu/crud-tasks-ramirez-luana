daee379ef3553915f1a2a9b4901255ccfdea0ae5
Luana Ramirez <luanaabigail168@gmail.com>
2025-08-17 18:32:41 -0300

    Cambie el timeTask por el userProfile

--- src/models/userProfile.model.js
+++ src/models/userProfile.model.js
@@ -1,22 +1,21 @@
 import { DataTypes } from "sequelize";
 import { sequelize } from "../config/database.js";
 
-export const Profile = sequelize.define(
-  "Profile",
+export const UserProfile = sequelize.define(
+  "UserProfile",
   {
     real_name: {
       type: DataTypes.STRING,
-      allowNull: false,
-    },
-    birthdate: {
-      type: DataTypes.DATEONLY
       allowNull: false,
     },
     bio: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-
+    birthdate: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
   },
   {
     timestamps: false,
