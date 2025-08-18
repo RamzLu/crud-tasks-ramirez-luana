41bfbf6a945a711e43e91ee016e3a6f4929968ad
Luana Ramirez <luanaabigail168@gmail.com>
2025-08-17 21:44:47 -0300

    intenté hacer el controlador para task

--- app.js
+++ app.js
@@ -3,17 +3,29 @@ import dotenv from "dotenv";
 dotenv.config();
 import { routerUser } from "./src/routers/user.router.js";
 import { startDB } from "./src/config/database.js";
-import { routerTask } from "./src/routers/task.router.js";
+// import { routerTask } from "./src/routers/task.router.js";
 import { labelTask } from "./src/models/labelTask.model.js";
 import { Labels } from "./src/models/label.model.js";
 import { UserProfile } from "./src/models/userProfile.model.js";
+import { Task } from "./src/models/task.model.js";
+import { User } from "./src/models/user.model.js";
 
 const PORT = process.env.PORT;
 const app = express();
 
 app.use(express.json());
 app.use("/api", routerUser);
-app.use("/api", routerTask);
+// app.use("/api", routerTask);
+app.get("/tasks", async (req, res) => {
+  const tasks = await Task.findAll({
+    include: [
+      {
+        model: User,
+        as: "author",
+      },
+    ],
+  });
+});
 
 app.listen(PORT, async () => {
   await startDB();
