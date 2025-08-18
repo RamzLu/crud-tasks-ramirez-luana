d74960321083d2885d1ca096346d450d99df7167
Ramirez Luana <luanaabigail168@gmail.com>
2025-08-18 11:50:44 -0300

    descomenté las rutas de Task

--- src/routers/task.router.js
+++ src/routers/task.router.js
@@ -1,15 +1,15 @@
-// import { Router } from "express";
-// import {
-//   createTask,
-//   deleteTask,
-//   getAllTask,
-//   getTaskById,
-//   upDatetask,
-// } from "../controller/task.controller.js";
-// export const routerTask = Router();
+import { Router } from "express";
+import {
+  createTask,
+  deleteTask,
+  getAllTask,
+  getTaskById,
+  upDatetask,
+} from "../controller/task.controller.js";
+export const routerTask = Router();
 
-// routerTask.get("/task", getAllTask);
-// routerTask.post("/task", createTask);
-// routerTask.get("/task/:id", getTaskById);
-// routerTask.put("/task/:id", upDatetask);
-// routerTask.delete("/task/:id", deleteTask);
+routerTask.get("/task", getAllTask);
+routerTask.post("/task", createTask);
+routerTask.get("/task/:id", getTaskById);
+routerTask.put("/task/:id", upDatetask);
+routerTask.delete("/task/:id", deleteTask);
