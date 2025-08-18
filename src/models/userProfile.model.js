000744bc7981bba8ccff16492298dc14b6818f5a
Luana Ramirez <luanaabigail168@gmail.com>
2025-08-17 21:46:48 -0300

    cambié la relacion de profile a user y las eliminé

--- src/models/userProfile.model.js
+++ src/models/userProfile.model.js
@@ -1,6 +1,5 @@
 import { DataTypes } from "sequelize";
 import { sequelize } from "../config/database.js";
-import { User } from "./user.model.js";
 
 export const UserProfile = sequelize.define(
   "UserProfile",
@@ -22,7 +21,3 @@ export const UserProfile = sequelize.define(
     timestamps: false,
   }
 );
-// el usuario solo tiene UN perfil
-User.hasOne(UserProfile, { foreignKey: "user_id" });
-// y un perfil se relaciona a UN usuario
-UserProfile.belongsTo(User, { foreignKey: "user_id" });
