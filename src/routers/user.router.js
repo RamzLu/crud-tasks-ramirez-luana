71a1bffcc477293a1b7c6325e5e0c27cc8a2e412
Ramirez Luana <luanaabigail168@gmail.com>
2025-08-25 11:08:57 -0300

    importé las validaciones a la ruta

--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -6,10 +6,15 @@ import {
   getUserById,
   upDateUser,
 } from "../controller/user.controller.js";
+import { validator } from "../middleware/validator.js";
+import {
+  createUserValidation,
+  updateUserValidation,
+} from "../middleware/validations/userValidations.js";
 export const routerUser = Router();
 
 routerUser.get("/user", getAllUsers);
-routerUser.post("/user", createUser);
+routerUser.post("/user", createUserValidation, validator, createUser);
 routerUser.get("/user/:id", getUserById);
-routerUser.put("/user/:id", upDateUser);
+routerUser.put("/user/:id", updateUserValidation, validator, upDateUser);
 routerUser.delete("/user/:id", deleteUser);
--- src/routers/userProfile.router.js
+++ src/routers/userProfile.router.js
@@ -7,10 +7,26 @@ import {
   daleteProfile,
 } from "../controller/userProfile.controller.js";
 
+import { validator } from "../middleware/validator.js";
+import {
+  createProfileValidation,
+  updateProfileValidation,
+} from "../middleware/validations/profileValidations.js";
+
 export const routerProfile = Router();
 
 routerProfile.get("/profiles", getAllprofiles);
 routerProfile.get("/profiles/:id", getProfileById);
-routerProfile.put("/profiles/:id", updateProfile);
-routerProfile.post("/profiles", createProfile);
+routerProfile.put(
+  "/profiles/:id",
+  updateProfileValidation,
+  validator,
+  updateProfile
+);
+routerProfile.post(
+  "/profiles",
+  createProfileValidation,
+  validator,
+  createProfile
+);
 routerProfile.delete("/profiles/:id", daleteProfile);
