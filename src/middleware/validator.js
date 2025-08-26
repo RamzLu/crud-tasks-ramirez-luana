988adf1ce67d81165d8f2a13f076b655c72973a2
Ramirez Luana <luanaabigail168@gmail.com>
2025-08-26 11:58:38 -0300

    añadí un status(400) al validator

--- src/middleware/validator.js
+++ src/middleware/validator.js
@@ -3,7 +3,7 @@ import { validationResult } from "express-validator";
 export const validator = (req, res, next) => {
   const result = validationResult(req);
   if (!result.isEmpty()) {
-    return res.json({ errors: result.mapped() });
+    return res.status(400).json({ errors: result.mapped() });
   }
   next();
 };
